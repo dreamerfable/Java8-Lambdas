c76c4eb7f0ae69c5509cad03c3c9074b74af96d8
dreamerfable <dreamerfable@gmail.com>
2018-08-23 21:59:05 +0800

    为Planet重写了 hashCode 和 equals 方法

--- lambdas/src/main/java/com/dreamerfable/lambdas/domain/Planet.java
+++ lambdas/src/main/java/com/dreamerfable/lambdas/domain/Planet.java
@@ -38,4 +38,32 @@ public class Planet {
 		return "planet [name=" + name + ", size=" + size + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + (int) (size ^ (size >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Planet other = (Planet) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (size != other.size)
+			return false;
+		return true;
+	}
+
 }
